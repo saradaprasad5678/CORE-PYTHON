23cf54248237612143f93f0e744ef5919957e99a
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-10 21:35:49 +0530

    Update Datatypes.py

--- Practical/Datatypes.py
+++ Practical/Datatypes.py
@@ -1,7 +1,17 @@
-Name='Prasad'
-age=21
-cgpa=8.2
-promoted='True'
+Name='Prasad' #string
+age=21 #int
+cgpa=8.2 #float
+promoted='True' #bool
+print('Name:',Name)
+print('Age:',agee)
+print('Cgpa:',cgpa)
+print('Promtoted:',promoted)
+
+#Dynamic input or user input
+Name=input("enter name") #string
+age=int(input("enter age"))#int
+cgpa=int(input("enter cgpa")) #float
+promoted=input("enter promotion") #bool
 print('Name:',Name)
 print('Age:',agee)
 print('Cgpa:',cgpa)
