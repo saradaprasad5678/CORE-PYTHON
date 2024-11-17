7f7512fe73272c0548bd4e3758ec0853897b9f4f
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-17 15:25:02 +0530

    Update swapping.py

--- Practical/swapping.py
+++ Practical/swapping.py
@@ -6,10 +6,3 @@ a=b
 b=c
 print("after swapping a=",a,"b=",b)
 
-#swapping without using a temporary variable
-a=int(input("enter first number:"))
-b=int(input("enter second number:"))
-a=a+b
-b=a-b
-a=a-b
-print("after swapping a=",a,"b=",b)
