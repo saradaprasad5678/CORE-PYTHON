85f3d15cdd06133eb7611470211683b5606918b2
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-18 09:34:57 +0530

    Update simple_if.py

--- Practical/simple_if.py
+++ Practical/simple_if.py
@@ -17,19 +17,3 @@ if 'h' not in 'hello':
     print('not divisible')
 else:
     print('available')
-
-#nested-if
-#largest of three numbers
-a=int(input('enter number:'))
-b=int(input('enter number:'))
-c=int(input('enter number:'))
-if a>b:
-    if a>c:
-        print('a is big')
-    else:
-        print('c is big')
-else:
-    if b>c:
-        print('b is big')
-    else:
-        print('c is big')
