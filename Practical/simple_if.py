d4aad24bbfea6644e35e76a7a941baa8bb42b611
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-18 09:33:41 +0530

    Update simple_if.py

--- Practical/simple_if.py
+++ Practical/simple_if.py
@@ -5,13 +5,6 @@ if x>18:
     print('eligible for voting')
 print(' not eligible for voting')    
     
-#to check odd number
-x=int(input('enter number:'))
-if x%2!=0:
-    print('odd')
-else:
-    print('even')
-    
 #not
 a=int(input('enter number:'))
 if  not (a%2==0 or a%3==0):
