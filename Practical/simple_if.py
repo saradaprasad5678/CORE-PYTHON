12aa5e32eb63a9703cedfcffedbc56d257ba5e2c
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-18 09:35:55 +0530

    Update simple_if.py

--- Practical/simple_if.py
+++ Practical/simple_if.py
@@ -5,15 +5,3 @@ if x>18:
     print('eligible for voting')
 print(' not eligible for voting')    
     
-#not
-a=int(input('enter number:'))
-if  not (a%2==0 or a%3==0):
-    print('not divisible')
-else:
-    print('divisible')
-    
-#not-in
-if 'h' not in 'hello':
-    print('not divisible')
-else:
-    print('available')
