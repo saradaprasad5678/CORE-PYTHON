3b4d9f6b4712f644447620f9fbe112b6da9b474c
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-18 09:40:23 +0530

    Update logical_operators.py

--- Practical/logical_operators.py
+++ Practical/logical_operators.py
@@ -12,3 +12,10 @@ if a%2==0 or a%3==0:
     print('divisible')
 else:
     print('not divisible')
+
+#not
+a=int(input('enter number:'))
+if  not a%2==0:
+    print('not divisible')
+else:
+    print('divisible')
