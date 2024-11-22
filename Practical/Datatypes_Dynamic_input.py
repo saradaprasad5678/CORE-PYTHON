a8a6db68645e3f4db014381e4a370533b27d34fb
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-11-22 09:43:30 +0530

    Create Datatypes_Dynamic_input.py

---
+++ Practical/Datatypes_Dynamic_input.py
@@ -0,0 +1,9 @@
+#Dynamic input or user input
+Name=input("enter name") #string
+age=int(input("enter age"))#int
+cgpa=int(input("enter cgpa")) #float
+promoted=input("enter promotion") #bool
+print('Name:',Name)
+print('Age:',agee)
+print('Cgpa:',cgpa)
+print('Promtoted:',promoted)
