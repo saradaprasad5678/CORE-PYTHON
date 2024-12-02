e2b76b764a4fe298a43bd351fb3590b0cef03bd0
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-12-02 19:31:00 +0530

    Create star_pattern_3.py

---
+++ Practical/star_pattern_3.py
@@ -0,0 +1,11 @@
+"""Write a python program to print star pattern
+    * 
+  * * 
+* * * """
+n=int(input('Enter:'))
+for i in range(n):
+  for j in range(n-i-1):
+    print(' ',end=' ')
+  for j in range(i+1):
+    print('*',end=' ')
+  print()
