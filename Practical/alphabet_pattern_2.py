a77f4994e862dfe13779af862b1ddf0b55ff80eb
Das Sarada Prasad <117529826+saradaprasad5678@users.noreply.github.com>
2024-12-15 15:08:10 +0530

    Create alphabet_pattern_2.py

---
+++ Practical/alphabet_pattern_2.py
@@ -0,0 +1,10 @@
+# Try to buy them program to print alphabet pattern
+#A A A
+#A A A
+#A A A
+k=65
+n=int(input('Enter: '))
+for i in range(n):
+  for j in range(n):
+    print(k,end=' ')
+  print()
